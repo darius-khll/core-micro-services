9db1d46aa5335ea63614c97eb25554bdc1ce858c
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-02 15:24:33 +0330

    calling another service in same network using httpClient

--- FirstService/Startup.cs
+++ FirstService/Startup.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace FirstService
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,7 +21,13 @@ namespace FirstService
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                HttpClient client = new HttpClient();
+
+                var res = await client.GetAsync("http://secondService");
+
+                string content = await res.Content.ReadAsStringAsync();
+
+                await context.Response.WriteAsync($"First Service requested: {content}");
             });
         }
     }
