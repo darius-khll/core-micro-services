e63381f734dcceff55daefe2ea979df2d7167ac4
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-03 18:19:04 +0330

    user model added

---
+++ SecondService/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SecondService.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
