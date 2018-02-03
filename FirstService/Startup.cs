8737d00bd769194fe355969a0c0a5405f6b72afb
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-03 17:24:52 +0330

    firstSevice startup enhanced by RedisManager client

--- FirstService/Startup.cs
+++ FirstService/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ServiceStack.Redis;
 using System.Net.Http;
 
 namespace FirstService
@@ -21,13 +22,19 @@ namespace FirstService
 
             app.Run(async (context) =>
             {
+                string foo = null;
+                RedisManagerPool manager = new RedisManagerPool("redis");
+                using (var client = manager.GetClient())
+                {
+                    client.Set("foo", "bar");
+                    foo = client.Get<string>("foo");
+                }
+
                 using (HttpClient client = new HttpClient())
                 {
                     var res = await client.GetAsync("http://secondService");
-
                     string content = await res.Content.ReadAsStringAsync();
-
-                    await context.Response.WriteAsync($"First Service requested: {content}");
+                    await context.Response.WriteAsync($"First Service requested: {content} - {foo}");
                 }
 
             });
