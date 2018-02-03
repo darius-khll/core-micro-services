ada8bd6c73494fdcd610f245d1bb8fc415f631a7
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-03 18:19:51 +0330

    Mongo client handler added to second service startup

--- SecondService/Startup.cs
+++ SecondService/Startup.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SecondService.Models;
+using System.Collections.Generic;
 
 namespace SecondService
 {
@@ -18,9 +21,17 @@ namespace SecondService
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Second Service");
+                //"mongodb://user:password@localhost"
+                MongoClient client = new MongoClient("mongodb://mongo"); //The client handles and dispose it automatically
+                IMongoDatabase db = client.GetDatabase("secondDb");
+
+                await db.GetCollection<User>("Users").InsertOneAsync(new User { Name = "abc1", Age = 10 });
+                List<User> users = (await db.GetCollection<User>("Users").FindAsync(c => c.Name == "abc1")).ToList();
+
+                await context.Response.WriteAsync($"Second Service respond a user named: {users[0].Name}");
             });
         }
     }
