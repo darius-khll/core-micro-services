47e5453f786b4a64796b3724eec589915a826f95
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-13 17:18:53 +0330

    redis options added

---
+++ FirstService/Implementations/RedisOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstService.Implementations
+{
+    public class RedisOptions
+    {
+        public string name { get; set; } = "localhost";
+
+        public int port { get; set; } = 6379;
+    }
+}
