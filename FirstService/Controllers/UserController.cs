c6e19ff38bfee5cb4326723601839abe398ceea6
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-13 17:18:29 +0330

    user controller added to firstService

---
+++ FirstService/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceStack.Redis;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FirstService.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly IRedisClient _client;
+        private readonly HttpClient _http;
+
+        public UserController(IRedisClient client, HttpClient http)
+        {
+            _client = client;
+            _http = http;
+        }
+
+        public async Task<string> Index()
+        {
+            _client.Set("foo", "bar123");
+            string foo = _client.Get<string>("foo");
+
+            var res = await _http.GetAsync("http://secondService");
+            string content = await res.Content.ReadAsStringAsync();
+
+            return string.Format($"First Service requested: {content} - {foo}");
+        }
+        
+    }
+}
