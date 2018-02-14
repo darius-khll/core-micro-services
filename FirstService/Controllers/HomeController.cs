a60bb0e643ba3a7cf8a63d499f8307fe519e6157
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-14 13:54:22 +0330

    error handler added

---
+++ FirstService/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Error()
+        {
+            return "Error happened!";
+        }
+    }
+}
