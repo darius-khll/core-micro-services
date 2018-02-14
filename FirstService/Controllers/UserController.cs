388897b71f4e568e22b31def9c8b70c8cde329db
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-14 13:54:38 +0330

    user controller enhanced

--- FirstService/Controllers/UserController.cs
+++ FirstService/Controllers/UserController.cs
@@ -1,32 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ServiceStack.Redis;
-using System.Net.Http;
+﻿using FirstService.Repository;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace FirstService.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IFirstBusiness _firstBusiness;
 
-        private readonly IRedisClient _client;
-        private readonly HttpClient _http;
-
-        public UserController(IRedisClient client, HttpClient http)
+        public UserController(IFirstBusiness firstBusiness)
         {
-            _client = client;
-            _http = http;
+            _firstBusiness = firstBusiness;
         }
 
         public async Task<string> Index()
         {
-            _client.Set("foo", "bar123");
-            string foo = _client.Get<string>("foo");
-
-            var res = await _http.GetAsync("http://secondService");
-            string content = await res.Content.ReadAsStringAsync();
-
-            return string.Format($"First Service requested: {content} - {foo}");
+            string name = await _firstBusiness.UserHandlerAsync("aaa", "bbb");
+            return name;
         }
-        
+
     }
 }
