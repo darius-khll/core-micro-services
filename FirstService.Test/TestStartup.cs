9a26a2da7ec467fb729750259cdb4580c6585579
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-17 21:10:42 +0330

    TestStartup added

---
+++ FirstService.Test/TestStartup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack.Redis;
+
+namespace FirstService.Test
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public override void ConfigureRedis(IServiceCollection services)
+        {
+            services.AddScoped(provider => new RedisManagerPool("localhost:5050").GetClient());
+        }
+    }
+}
