ff353f283b86c69523fc1c0147d301280e8c96e3
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-18 21:05:40 +0330

    cache controller added

---
+++ FirstService/Controllers/CacheController.cs
@@ -0,0 +1,30 @@
+﻿using FirstService.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FirstService.Controllers
+{
+    public class CacheController : Controller
+    {
+        private readonly ICacheBusiness _cacheBusiness;
+
+        public CacheController(ICacheBusiness cacheBusiness)
+        {
+            _cacheBusiness = cacheBusiness;
+        }
+
+        [HttpGet]
+        public async Task<string> Index()
+        {
+            string result = await _cacheBusiness.CacheIfNotExist();
+            return result;
+        }
+
+        [HttpGet]
+        public async Task<string> Del()
+        {
+            string result = await _cacheBusiness.RemoveCache();
+            return result;
+        }
+    }
+}
