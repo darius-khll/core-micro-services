dde5f14b51fe4a167e5a100fefb7b355f26d8a32
Ali Khalili <ali.khll.1994@gmail.com>
2018-02-22 20:07:05 +0330

    oauth server configure service enhanced

--- OAuthServer/Startup.cs
+++ OAuthServer/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OAuthServer.Configurations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OAuthServer
 {
@@ -10,8 +11,14 @@ namespace OAuthServer
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            //C:\Users\ali\source\repos\CoreMicroServices\OAuthServer\socialnetwork.pfx
+
+            //openssl req -newkey rsa:2048 -nodes -keyout socialnetwok.key -x509 -days 365 -out socialnetwork.cer
+            //you may need try this command befor execute next one => winpty bash
+            //openssl pkcs12 -export -in socialnetwork.cer - inkey socialnetwok.key -out socialnetwork.pfx
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
+                //.AddDeveloperSigningCredential()
+                .AddSigningCredential(new X509Certificate2(@"/app/socialnetwork.pfx", "password")) //password given from pfx file which genrated by cli
                 .AddTestUsers(InMemoryConfiguration.Users())
                 .AddInMemoryClients(InMemoryConfiguration.Clients())
                 .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
