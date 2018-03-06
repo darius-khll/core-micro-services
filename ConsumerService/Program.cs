7c73e8837d4fab415919820c8f982d888067bdc6
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-06 15:49:55 +0330

    consume service added

---
+++ ConsumerService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
