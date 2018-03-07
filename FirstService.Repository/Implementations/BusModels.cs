4cf3aef5a337b225f73dae74818d8f69900aeee9
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-07 13:18:34 +0330

    bus model added

---
+++ FirstService.Repository/Implementations/BusModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstService.Repository.Implementations
+{
+    public interface SubmitOrder
+    {
+        string OrderId { get; }
+    }
+
+    public interface OrderAccepted
+    {
+        string OrderId { get; }
+    }
+}
