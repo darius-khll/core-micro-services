53a17c0b672e96dea3a6cfccd6172cf7cab667fc
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-07 13:13:42 +0330

    consumer service masstransit + mongo package added

--- ConsumerService/Program.cs
+++ ConsumerService/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using FirstService.Repository.Implementations;
+using MassTransit;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ConsumerService
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://rabbitmq/"), h => { });
+
+                cfg.ReceiveEndpoint(host, "order-service", e =>
+                {
+                    e.Handler<SubmitOrder>(context => context.RespondAsync<OrderAccepted>(new
+                    {
+                        context.Message.OrderId
+                    }));
+                });
+            });
+
+            await bus.StartAsync();
+            try
+            {
+                Console.WriteLine("Working....");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                await bus.StopAsync();
+            }
+        }
+
+        async Task Mongo()
+        {
+            //"mongodb://user:password@localhost"
+            MongoClient client = new MongoClient("mongodb://mongo"); //The client handles and dispose it automatically
+            IMongoDatabase db = client.GetDatabase("secondDb");
+
+            await db.GetCollection<User>("Users").InsertOneAsync(new User { Name = "abc1", Age = 10 });
+            List<User> users = (await db.GetCollection<User>("Users").FindAsync(c => c.Name == "abc1")).ToList();
+
+            Console.WriteLine(users[0].Name);
         }
     }
 }
