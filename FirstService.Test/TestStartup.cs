2cd584afba18a528a880efa6e8702653f3e8f76e
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-08 16:42:28 +0330

    ConfigureRabbitmqHost test startup fixed

--- FirstService.Test/TestStartup.cs
+++ FirstService.Test/TestStartup.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
+using System;
 
 namespace FirstService.Test
 {
     public class TestStartup : Startup
     {
         public const string RediHost = "localhost:8184";
+        public const string RabbitmqHost = "localhost:5672";
 
         public TestStartup(IConfiguration configuration) : base(configuration)
         {
+        }
+
+        public override IBusControl ConfigureRabbitmqHost(IServiceCollection services)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost:5672/"), h =>
+                {
+                    //h.Username("guest");
+                    //h.Password("guest");
+                });
+            });
+
+            return bus;
         }
 
         public override void ConfigureRedis(IServiceCollection services)
