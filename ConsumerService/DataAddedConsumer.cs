561e0323719b968514dce6a638a59a747e931020
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-25 15:10:00 +0430

    dataAdded consumer + it's falut service added

---
+++ ConsumerService/DataAddedConsumer.cs
@@ -0,0 +1,36 @@
+﻿using Common.Implementations;
+using FirstService.Repository.Implementations;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsumerService
+{
+    public class DataAddedConsumer : IConsumer<IPubSub>
+    {
+        public readonly IHttpService _httpService;
+
+        public DataAddedConsumer(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public async Task Consume(ConsumeContext<IPubSub> context)
+        {
+            var user = _httpService.ToString();
+
+            if (1 == 2) throw new Exception("Error1"); //goes to DataAddedFaultConsumer (if you have retry policy first restart happens!)
+
+            await Task.Delay(5000);
+        }
+    }
+
+    //when exeption raises
+    public class DataAddedFaultConsumer : IConsumer<Fault<IPubSub>>
+    {
+        public async Task Consume(ConsumeContext<Fault<IPubSub>> context)
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
