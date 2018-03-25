e2a14e9c8edf16876fae9cb7652402d672fc833d
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-25 15:11:47 +0430

    SubmitOrder consumer added

---
+++ ConsumerService/SubmitOrderConsumer.cs
@@ -0,0 +1,17 @@
+﻿using FirstService.Repository.Implementations;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace ConsumerService
+{
+    public class SubmitOrderConsumer : IConsumer<SubmitOrder>
+    {
+        public Task Consume(ConsumeContext<SubmitOrder> context)
+        {
+            return context.RespondAsync<OrderAccepted>(new
+            {
+                context.Message.OrderId
+            });
+        }
+    }
+}
