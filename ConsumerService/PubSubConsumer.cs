1ff8491697a5abe8b2a8679ec994989338febfb6
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-25 15:11:32 +0430

    PubSub Consumer added

---
+++ ConsumerService/PubSubConsumer.cs
@@ -0,0 +1,14 @@
+﻿using FirstService.Repository.Implementations;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace ConsumerService
+{
+    public class PubSubConsumer : IConsumer<IPubSub>
+    {
+        public async Task Consume(ConsumeContext<IPubSub> context)
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
