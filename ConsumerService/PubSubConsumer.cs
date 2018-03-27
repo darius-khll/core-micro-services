b4f22d4c68245b258ec1c1df2fb44e9440ee590a
Ali Khalili <ali.khll.1994@gmail.com>
2018-03-27 21:59:27 +0430

    pubSub consumer enhanced to mongo collection

--- ConsumerService/PubSubConsumer.cs
+++ ConsumerService/PubSubConsumer.cs
@@ -1,13 +1,28 @@
 ﻿using FirstService.Repository.Implementations;
 using MassTransit;
+using MongoDB.Driver;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConsumerService
 {
     public class PubSubConsumer : IConsumer<IPubSub>
     {
+        public IMongoClient _mongoClient { get; }
+
+        public PubSubConsumer(IMongoClient mongoClient)
+        {
+            _mongoClient = mongoClient;
+        }
+
         public async Task Consume(ConsumeContext<IPubSub> context)
         {
+            IMongoDatabase db = _mongoClient.GetDatabase("secondDb");
+
+            await db.GetCollection<User>("Users").InsertOneAsync(new User { Name = "abc1", Age = 10 });
+
+            List<User> users = (await db.GetCollection<User>("Users").FindAsync(c => c.Name == "abc1")).ToList();
+
             await Task.Delay(5000);
         }
     }
