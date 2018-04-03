b72586e666b3a9e0351512cd12ea8adf2b95d909
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-03 20:14:03 +0430

    Custome route attribute added

---
+++ Common/Filters/CustomRouteAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Common.Filters
+{
+    public class CustomRouteAttribute : RouteAttribute
+    {
+        private static string _prefix = "/api/[controller]";
+        public CustomRouteAttribute() : base(_prefix)
+        {
+        }
+    }
+}
