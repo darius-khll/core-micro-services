cb7352b313a35ddff66f5ce6c6137404eceb228c
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-04 16:21:31 +0430

    query attribute added

---
+++ Common/Filters/QueryAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace Common.Filters
+{
+    public class QueryAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
