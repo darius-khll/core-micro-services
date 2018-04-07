fa8832526c41d3b73579e2ca8b72abc597b32127
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-07 18:24:46 +0430

    events enhanced

---
+++ ConsumerService/Events/RequestResponse/SubmitOrderConsumer.cs
@@ -0,0 +1,19 @@
+﻿using FirstService.Repository.Implementations;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace ConsumerService.Consumers
+{
+    public class SubmitOrderConsumer : IConsumer<SubmitOrder>
+    {
+        public async Task Consume(ConsumeContext<SubmitOrder> context)
+        {
+            await Task.Delay(1000);
+
+            await context.RespondAsync<OrderAccepted>(new
+            {
+                context.Message.OrderId
+            });
+        }
+    }
+}
