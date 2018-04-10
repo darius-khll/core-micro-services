66e8c4b7fab603a89c6377625a10aec98b985c38
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-10 15:51:09 +0430

    service bus controller enhanced

--- FirstService/Controllers/ServiceBusController.cs
+++ FirstService/Controllers/ServiceBusController.cs
@@ -1,9 +1,8 @@
 ﻿using Common.Filters;
-using Common.Options;
+using Common.Repositories.ServiceBus;
 using ConsumerService.Business.Models;
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,32 +13,31 @@ namespace FirstService.Controllers
     public class ServiceBusController : Controller
     {
         private readonly IRequestClient<SubmitOrder, OrderAccepted> _requestClient;
-        private readonly IBus _bus;
-        private RabbitmqOptions _rabbitmqOptions { get; }
+        public readonly IServiceBusRepository _busRepository;
 
         public ServiceBusController(IRequestClient<SubmitOrder, OrderAccepted> requestClient,
-            IBus bus, IOptions<RabbitmqOptions> rabbitmqOptions)
+            IServiceBusRepository busRepository)
         {
             _requestClient = requestClient;
-            _bus = bus;
-            _rabbitmqOptions = rabbitmqOptions.Value;
+            _busRepository = busRepository;
         }
+
+
         /*
-         *  AddToServiceBus => 
-         *      1) publish
-         *      2) using endpoint for specefic client
-         *      3) request/response
-         */
+        *  AddToServiceBus => 
+        *      1) publish
+        *      2) using endpoint for specefic client
+        *      3) request/response
+        */
         [HttpGet]
         [Route(nameof(AddToServiceBus))]
         public async Task<string> AddToServiceBus(CancellationToken cancellationToken)
         {
             //1
-            await _bus.Publish<IPubSub>(new PubSub { Message = "send message" });
+            await _busRepository.Publish<IPubSub>(new PubSub { Message = "send message" });
 
             //2
-            var endpoint = await _bus.GetSendEndpoint(new Uri($"rabbitmq://{_rabbitmqOptions.host}/data-added"));
-            await endpoint.Send<IPubSub>(new PubSub { Message = "data passed" });
+            await _busRepository.SendToEndpoint<IPubSub>("data-added", new PubSub { Message = "data passed" });
 
             //3
             OrderAccepted result = await _requestClient.Request(new { OrderId = 123 }, cancellationToken);
