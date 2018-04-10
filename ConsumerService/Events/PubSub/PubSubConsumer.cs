9c0d585fddf32b646bb13ad65e658e6c5badac42
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-10 15:50:23 +0430

    PubSub consumer enhanced

--- ConsumerService/Events/PubSub/PubSubConsumer.cs
+++ ConsumerService/Events/PubSub/PubSubConsumer.cs
@@ -1,29 +1,22 @@
-﻿using Common.Repositories.Mongo;
+﻿using ConsumerService.Business;
 using ConsumerService.Business.Models;
 using MassTransit;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConsumerService.Consumers
 {
     public class PubSubConsumer : IConsumer<IPubSub>
     {
-        public IMongoRepository<User> _mongoRepository { get; set; }
+        public IMongoBusiness _mongoBusiness { get; set; }
 
-        public PubSubConsumer(IMongoRepository<User> mongoRepository)
+        public PubSubConsumer(IMongoBusiness mongoBusiness)
         {
-            _mongoRepository = mongoRepository;
+            _mongoBusiness = mongoBusiness;
         }
 
         public async Task Consume(ConsumeContext<IPubSub> context)
         {
-            User savedUser = await _mongoRepository.SaveAsync(new User { Name = "ali", Age = 20, Addresses = new List<string> { "Tehran", "Shiraz" } });
-
-            User takenUser = await _mongoRepository.GetByIdAsync(savedUser.Id);
-
-            ICollection<User> users = await _mongoRepository.FindAllAsync(u => u.Name == "ali");
-
-            await _mongoRepository.DeleteAsync(takenUser.Id);
+            await _mongoBusiness.HandleLogic();
         }
     }
 }
