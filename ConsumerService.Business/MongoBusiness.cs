1b74340698038219fb238c155ed40fa1efde3ee0
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-10 15:50:58 +0430

    mongo business added

---
+++ ConsumerService.Business/MongoBusiness.cs
@@ -0,0 +1,33 @@
+﻿using Common.Repositories.Mongo;
+using ConsumerService.Business.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ConsumerService.Business
+{
+    public interface IMongoBusiness
+    {
+        Task HandleLogic();
+    }
+
+    public class MongoBusiness : IMongoBusiness
+    {
+        public IMongoRepository<User> _mongoRepository { get; set; }
+
+        public MongoBusiness(IMongoRepository<User> mongoRepository)
+        {
+            _mongoRepository = mongoRepository;
+        }
+
+        public async Task HandleLogic()
+        {
+            User savedUser = await _mongoRepository.SaveAsync(new User { Name = "ali", Age = 20, Addresses = new List<string> { "Tehran", "Shiraz" } });
+
+            User takenUser = await _mongoRepository.GetByIdAsync(savedUser.Id);
+
+            ICollection<User> users = await _mongoRepository.FindAllAsync(u => u.Name == "ali");
+
+            await _mongoRepository.DeleteAsync(takenUser.Id);
+        }
+    }
+}
