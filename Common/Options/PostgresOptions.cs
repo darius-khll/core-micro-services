3c4f6ec0369715c64ffee1c45772559690456335
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-15 16:27:00 +0430

    postgres options added

---
+++ Common/Options/PostgresOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Options
+{
+    public class PostgresOptions : ISettingOptions
+    {
+        public string host { get; set; }
+        public string name { get; set; }
+        public int port { get; set; }
+
+        public static string GetConfigName => "postgres";
+    }
+}
