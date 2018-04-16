df3c04e62661bc8ddfa57d59f7f9bdaab714bd65
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-16 16:45:38 +0430

    pubSub event enhanced

--- ConsumerService/Events/PubSub/PubSubConsumer.cs
+++ ConsumerService/Events/PubSub/PubSubConsumer.cs
@@ -7,16 +7,19 @@ namespace ConsumerService.Consumers
 {
     public class PubSubConsumer : IConsumer<IPubSub>
     {
-        public IMongoBusiness _mongoBusiness { get; set; }
+        public readonly IMongoBusiness _mongoBusiness;
+        public readonly IDapperBusiness _dapperBusiness;
 
-        public PubSubConsumer(IMongoBusiness mongoBusiness)
+        public PubSubConsumer(IMongoBusiness mongoBusiness, IDapperBusiness dapperBusiness)
         {
             _mongoBusiness = mongoBusiness;
+            _dapperBusiness = dapperBusiness;
         }
 
         public async Task Consume(ConsumeContext<IPubSub> context)
         {
             await _mongoBusiness.HandleLogic();
+            await _dapperBusiness.HandleLogic();
         }
     }
 
