49a15cf44474d97105397803f3f0d647f2401b35
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-17 17:35:20 +0430

    service bus request adding ro scoped extension method added

--- Common/Implementations/ServiceBusExtensions.cs
+++ Common/Implementations/ServiceBusExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using MassTransit;
 using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +14,15 @@ namespace Common.Implementations
         /// <summary>
         ///     endpoint should be name of Consumer
         /// </summary>
+        /// <param name="cfg"></param>
         /// <param name="host">host</param>
         /// <param name="context">context</param>
         /// <param name="namespaces">find consumer in specefic namespaces </param>
         /// <param name="onConfigure">overrided configs</param>
         public static void AddConsumersEndpoint(this IRabbitMqBusFactoryConfigurator cfg, IRabbitMqHost host, IComponentContext context, string[] namespaces, Func<TypeInfo, IRabbitMqReceiveEndpointConfigurator, bool> onConfigure = null)
         {
-            MethodInfo autofacConsumerMethod = typeof(AutofacExtensions).GetTypeInfo().GetMethod(nameof(AutofacExtensions.Consumer), new[] { typeof(IReceiveEndpointConfigurator).GetTypeInfo(), typeof(IComponentContext).GetTypeInfo(), typeof(string).GetTypeInfo() });
+            MethodInfo autofacConsumerMethod = typeof(AutofacExtensions).GetTypeInfo()
+                .GetMethod(nameof(AutofacExtensions.Consumer), new[] { typeof(IReceiveEndpointConfigurator).GetTypeInfo(), typeof(IComponentContext).GetTypeInfo(), typeof(string).GetTypeInfo() });
 
             List<TypeInfo> consumersTypes = AppDomain.CurrentDomain.GetAssemblies()
                .Where(asm => asm.IsDynamic == false)
@@ -43,6 +46,49 @@ namespace Common.Implementations
                     if (shouldRegisterConsumer)
                         autofacConsumerMethod.MakeGenericMethod(consumerType).Invoke(e, new object[] { e, context, "message" });
                 });
+            }
+        }
+
+        public static void f1(IServiceCollection services, string rabbitmqHost, TimeSpan timeout, string[] namespaces)
+        {
+
+            List<TypeInfo> consumersTypes = AppDomain.CurrentDomain.GetAssemblies()
+               .Where(asm => asm.IsDynamic == false)
+               .Where(asm => asm.GetReferencedAssemblies().Any(refAsm => refAsm.Name == "MassTransit"))
+               .SelectMany(x => x.GetExportedTypes())
+               .Where(x => typeof(IRequestResponse).IsAssignableFrom(x) && x.IsClass && !x.IsAbstract)
+               .Where(x => namespaces.Contains(x.Namespace))
+               .Select(x => x.GetTypeInfo())
+               .ToList();
+
+            foreach (TypeInfo consType in consumersTypes)
+            {
+                Type responseType = null, requestType = null;
+
+                foreach (var genericType in consType.GetInterfaces())
+                {
+
+                    if (genericType.IsGenericType && genericType.GetGenericTypeDefinition() == typeof(IRequestResponse<>))
+                    {
+                        responseType = genericType.GetGenericArguments()[0]; //response type
+                    }
+                    else if (genericType.IsGenericType && genericType.GetGenericTypeDefinition() == typeof(IConsumer<>))
+                    {
+                        requestType = genericType.GetGenericArguments()[0]; //request type
+                    }
+                }
+
+                Uri serviceAddress = new Uri($"rabbitmq://{rabbitmqHost}/{consType.Name}");
+
+                var requestClientGenericType = typeof(IRequestClient<,>).MakeGenericType(new Type[] { requestType, responseType });
+
+                ServiceCollectionServiceExtensions.AddScoped(services, requestClientGenericType, new Func<IServiceProvider, object>(sp =>
+                {
+                    return Activator.CreateInstance(typeof(MessageRequestClient<,>).MakeGenericType(requestType, responseType),
+                        args: new object[] {
+                            sp.GetRequiredService<IBus>(), serviceAddress, timeout, timeout
+                        });
+                }));
             }
         }
     }
