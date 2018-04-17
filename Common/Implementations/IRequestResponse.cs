66752d41116e963bee4c3b75dbef225077a5b777
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-17 17:34:53 +0430

    IRequestResponse marker added

---
+++ Common/Implementations/IRequestResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Implementations
+{
+    /// <summary>
+    /// just a marker to detect requestResponse's consumer
+    /// T is return type of consumer
+    /// </summary>
+    public interface IRequestResponse<T> : IRequestResponse where T : class
+    {
+    }
+
+    public interface IRequestResponse
+    {
+    }
+}
