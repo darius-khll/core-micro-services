4a4f04e0d4b46d8e26c0600e8d9a10791b161757
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-21 16:24:42 +0430

    efcore appDbContext added

---
+++ Common/Repositories/Postgres/EfCore/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Common.Repositories.Postgres.EfCore
+{
+    //dotnet ef  migrations add CreateDatabase
+    //dotnet ef database update
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=localhost; Port=8189; User Id=postgres; Password=; database=AppDbCcontext");
+        }
+    }
+
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
