0dc5b78abbf049bbd5892fed23403a7251f6e270
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-22 13:15:10 +0430

    models seperation fixed

--- Common/Repositories/Postgres/EfCore/AppDbContext.cs
+++ Common/Repositories/Postgres/EfCore/AppDbContext.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Common.Repositories.Postgres.EfCore
 {
@@ -27,23 +24,5 @@ namespace Common.Repositories.Postgres.EfCore
             //could be UseSqlServer
             optionsBuilder.UseNpgsql(@"Server=localhost; Port=8189; User Id=postgres; Password=; database=AppDbContext");
         }
-    }
-
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Family { get; set; }
-    }
-    public class Address
-    {
-        [Key]
-        public Guid Id { get; set; }
-
-        public string Title { get; set; }
-
-        [ForeignKey(nameof(UserId))]
-        public virtual User User { get; set; }
-        public Guid UserId { get; set; }
     }
 }
---
+++ Common/Repositories/Postgres/EfCore/Models.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Common.Repositories.Postgres.EfCore
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+    public class Address
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
