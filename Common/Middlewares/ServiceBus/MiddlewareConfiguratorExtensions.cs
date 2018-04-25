b384e94b83736cb78a9ea4235a1461e595d5490e
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-25 20:38:16 +0430

    MiddlewareConfiguratorExtensions added

---
+++ Common/Middlewares/ServiceBus/MiddlewareConfiguratorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GreenPipes;
+
+namespace Common.Middlewares.ServiceBus
+{
+    public static class MiddlewareConfiguratorExtensions
+    {
+        public static void UseExceptionLogger<T>(this IPipeConfigurator<T> configurator)
+            where T : class, PipeContext
+        {
+            configurator.AddPipeSpecification(new ExceptionLoggerSpecification<T>());
+        }
+
+        public static void UseLogger<T>(this IPipeConfigurator<T> configurator)
+            where T : class, PipeContext
+        {
+            configurator.AddPipeSpecification(new LoggerSpecification<T>());
+        }
+    }
+}
