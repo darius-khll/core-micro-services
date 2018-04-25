3a00756aa89e16f6f857747d5e1e64035f6142c5
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-25 20:38:00 +0430

    logger filter added

---
+++ Common/Middlewares/ServiceBus/LoggerFilter.cs
@@ -0,0 +1,47 @@
+﻿using GreenPipes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Middlewares.ServiceBus
+{
+    public class LoggerSpecification<T> : IPipeSpecification<T> where T : class, PipeContext
+    {
+        public IEnumerable<ValidationResult> Validate()
+        {
+            return Enumerable.Empty<ValidationResult>();
+        }
+
+        public void Apply(IPipeBuilder<T> builder)
+        {
+            builder.AddFilter(new LoggerFilter<T>());
+        }
+    }
+
+
+    public class LoggerFilter<T> : IFilter<T> where T : class, PipeContext
+    {
+        public void Probe(ProbeContext context)
+        {
+        }
+
+        public async Task Send(T context, IPipe<T> next)
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("request started!");
+            //string a = ((IMyInterface)context).DestinationAddress;
+
+            await next.Send(context);
+
+            Console.WriteLine("request finished!");
+            Console.WriteLine("--------------------");
+
+        }
+    }
+
+    public interface IMyInterface : PipeContext
+    {
+        string DestinationAddress { get; }
+    }
+}
