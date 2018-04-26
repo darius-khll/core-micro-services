0c97ffeb56ecb2f9dc854b93481749ede92a4451
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-26 17:22:27 +0430

    appDbContext seed data added

---
+++ Common/Repositories/Postgres/EfCore/AppDbContextSeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Repositories.Postgres.EfCore
+{
+    public static class AppDbContextSeedData
+    {
+        public static void SeedData(DbContextOptions<AppDbContext> options)
+        {
+            using (var context = new AppDbContext(options))
+            {
+                if (!context.Users.Any())
+                {
+                    var persons = new List<User>
+                    {
+                        new User
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Admin",
+                            Family = "User"
+                        }
+                    };
+                    context.AddRange(persons);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
