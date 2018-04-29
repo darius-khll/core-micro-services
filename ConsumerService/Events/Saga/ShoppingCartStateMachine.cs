a8345f68beeefa848bb450396da91d8074504776
Ali Khalili <ali.khll.1994@gmail.com>
2018-04-29 18:39:52 +0430

    Shopping card sagas imnplementation added

---
+++ ConsumerService/Events/Saga/ShoppingCartStateMachine.cs
@@ -0,0 +1,21 @@
+﻿using Automatonymous;
+using System;
+
+namespace ConsumerService.Events.Saga
+{
+    public class ShoppingCartStateMachine : MassTransitStateMachine<ShoppingCart>
+    {
+        public ShoppingCartStateMachine()
+        {
+        }
+    }
+
+    public class ShoppingCart : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public State CurrentState { get; set; }
+        public DateTime ReceivedDateTime { get; set; }
+        public DateTime RegisteredDateTime { get; set; }
+        public string PickupName { get; set; }
+    }
+}
